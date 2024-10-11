8fc899448eff1bdb1c29c5b14fd1a98ed5800e32
http.dre <diaso.andre@outlook.com>
2024-10-11 19:12:36 -0300

    adds the 'key' atribute to the 'files' table

--- seed.sql
+++ seed.sql
@@ -3,5 +3,6 @@ CREATE TABLE IF NOT EXISTS files (
     filename TEXT NOT NULL,
     path TEXT NOT NULL,
     filetype TEXT NOT NULL,
-    createdAtUTC TEXT NOT NULL
+    createdAtUTC TEXT NOT NULL,
+    key TEXT UNIQUE NOT NULL
 );
